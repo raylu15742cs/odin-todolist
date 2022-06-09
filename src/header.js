076b137c0e50c61e9257131ebb56a4e540a41aef
raylu15742cs <raylu15742cs@gmail.com>
2022-06-09 15:49:33 -0700

    finished home button

--- src/header.js
+++ src/header.js
@@ -5,34 +5,11 @@ export default function headertabs() {
     const lefttab = document.createElement("div");
     let sidebaropen = document.createElement("button")
     sidebaropen.innerHTML = '<img src="../img/three-dash.png"></img>'
-    let count = 0;
-    sidebaropen.addEventListener("click", function() {
-        let hider = document.getElementById("sidebar")
-        let main = document.getElementById("maincontent")
-        if(count%2 != 0) {
-            hider.style.visibility = 'visible';
-            main.style.width = "80vw";
-            hider.style.width = "20vw";
-            count++;
-        } else {
-            hider.style.visibility ="hidden";
-            main.style.width = "100vw";
-            hider.style.width = "0vw";
-            count++;
-        }
-    })
+    sidebaropen.addEventListener("click", sidebarcontrol)
     lefttab.appendChild(sidebaropen)
     let homebutton = document.createElement("button")
     homebutton.innerHTML = '<img src="../img/home.png"></img>'
-    homebutton.addEventListener("click", function() {
-        let hider = document.getElementById("sidebar")
-        let main = document.getElementById("maincontent")
-        count = 1;
-        hider.style.visibility = 'visible';
-        main.style.width = "80vw";
-        hider.style.width = "20vw";
-        count++;
-    })
+    homebutton.addEventListener("click", homebuttonsfunction )
     lefttab.appendChild(homebutton)
     const righttab = document.createElement("div");
     let addnew = document.createElement("button")
@@ -47,4 +24,26 @@ export default function headertabs() {
     headertab.appendChild(righttab)
 
 
+}
+let count = 0
+let homebuttonsfunction = function() {
+    count = 1
+    sidebarcontrol()
+}
+let sidebarcontrol = function() {
+    let hider = document.getElementById("sidebar")
+    let main = document.getElementById("maincontent")
+    if(count%2 != 0) {
+        hider.style.visibility = 'visible';
+        main.style.width = "80vw";
+        hider.style.width = "20vw";
+        hider.style.padding = "4vh 2vw 0 2vw"
+        count++;
+    } else {
+        hider.style.visibility ="hidden";
+        main.style.width = "100vw";
+        hider.style.width = "0vw";
+        hider.style.padding = "0"
+        count++;
+    }
 }
